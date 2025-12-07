293c768e3114fda67a53262e2af34141fad4d006
Vitoria Almeida <almeidademariavitoria@gmail.com>
2025-12-07 11:53:52 -0300

    @zoo 30 min

--- tasks/poo/zoo/py/draft.py
+++ tasks/poo/zoo/py/draft.py
@@ -1,4 +1,26 @@
+
+
 class Animal:
-    def __init__(self, name):
-        self.__name = name
-        
+    def __init__(self, nome):
+        self.__nome = nome
+    
+    def apresentar_nome(self):
+        print(f"Eu sou um(a) {self.__nome}")
+
+    def fazerSom(self):
+        return "som"
+
+class Elefante(Animal):
+    def __init__(self, nome):
+        super().__init__(nome)
+
+    def fazerSom(self):
+        return "brummm"
+
+
+elefante = Elefante("dumbo")
+elefante.apresentar_nome()
+print (elefante.fazerSom())
+    
+
+
