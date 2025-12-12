6904e51f5a64a7e54c60d3a3c06cd4b8146a92e1
Vitoria Almeida <almeidademariavitoria@gmail.com>
2025-12-12 09:46:54 -0300

    @estacionamento class Veiculo finalizada 50 min

---
+++ tasks/poo/estacionamento/py/draft.py
@@ -0,0 +1,26 @@
+from abc import ABC, abstractmethod
+
+class Veiculo(ABC):
+    def __init__(self, id:str, tipo:str):
+        self.__id: str = id 
+        self._tipo: str = tipo
+        self._horaEntrada: int 
+    
+    def setEntrada(self, horaEntrada:int ) -> None:
+        self._horaEntrada = horaEntrada
+
+    def getEntrada(self) -> int:
+        return self._horaEntrada
+    
+    def getTipo(self) -> str:
+        return self._tipo
+    
+    def getId(self) -> str:
+        return self.__id
+    
+    @abstractmethod
+    def calcularValor(self, horaSair: int) -> None:
+        pass
+
+    def __str__(self) -> str:
+        return f"{self._tipo.rjust(10, "_")}: {self.__id.rjust(10, "_")}: {self._horaEntrada}"
